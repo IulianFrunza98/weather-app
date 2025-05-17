f9e49ede29da5711232f3a97d78a76e627d7817a
IulianFrunza98 <fiulian87@yahoo.com>
2025-05-17 20:46:08 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ import { updateWeatherData } from "./main.js";
 // Api call
 
 export async function checkWeather(city) {
-  const apiKey = "13187f3fc99f65ff67d120d87213b972";
+  const apiKey = "";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}`;
 
   const loadingIndicator = document.getElementById("loading-indicator");
