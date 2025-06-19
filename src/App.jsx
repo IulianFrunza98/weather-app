8da2a2625f42f1567c4929ec1bf4e2a411283ad0
IulianFrunza98 <fiulian87@yahoo.com>
2025-06-19 19:04:23 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { WeatherProvider } from "./contexts/WeatherContext";
 
 import { WeatherDisplay } from "./components/WeatherDisplay";
 import { Search } from "./components/Search";
+import styled from "styled-components";
 
 function App() {
   return (
     <WeatherProvider>
+      <Title>Weather App</Title>
       <Search />
       <WeatherDisplay />
     </WeatherProvider>
@@ -13,3 +15,8 @@ function App() {
 }
 
 export default App;
+
+const Title = styled.h1`
+  font-size: 2.4rem;
+  text-align: center;
+`;
